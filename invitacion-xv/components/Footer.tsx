b3ab0e86afd735fb57e0f9a7a8dc62c80d3332da
luiscantuXV <luiscantu.311@gmail.com>
2026-07-08 14:10:35 -0600

    Footer premium agregado

--- invitacion-xv/components/Footer.tsx
+++ invitacion-xv/components/Footer.tsx
@@ -1,16 +1,46 @@
+
+import { quinceanera } from "../data/quinceanera";
+
 export default function Footer() {
   return (
     <footer
       style={{
+        background: "#111111",
+        color: "#ffffff",
         textAlign: "center",
-        padding: "50px 20px",
-        background: "#111",
-        color: "white",
+        padding: "80px 20px",
       }}
     >
-      <h3>Angela Shakti</h3>
+      <h2
+        style={{
+          color: "#d4af37",
+          fontSize: "2.5rem",
+          marginBottom: "20px",
+        }}
+      >
+        {quinceanera.nombre}
+      </h2>
 
-      <p>Gracias por acompañarme en este día tan especial</p>
+      <p
+        style={{
+          fontSize: "1.2rem",
+          maxWidth: "600px",
+          margin: "0 auto",
+          lineHeight: "1.8",
+        }}
+      >
+        Gracias por acompañarme en este día tan especial.
+        Tu presencia hará de este momento un recuerdo inolvidable.
+      </p>
+
+      <div
+        style={{
+          marginTop: "40px",
+          color: "#888",
+        }}
+      >
+        Mis XV Años © 2026
+      </div>
     </footer>
   );
 }
